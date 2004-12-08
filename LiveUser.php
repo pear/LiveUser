65aa2fa42f89da3f67a35eb3ffa1dcdacb346242
Arnaud Limbourg <arnaud@php.net>
2004-12-08 19:56:03 +0000

    parse error
    git-svn-id: http://svn.php.net/repository/pear/packages/LiveUser/trunk@174299 c90b9560-bf6c-de11-be94-00142212c4b1

--- LiveUser.php
+++ LiveUser.php
@@ -1494,7 +1494,7 @@ class LiveUser
         $num = count($this->_observers[$event]);
         for ($i = 0; $i < $num; $i++) {
             if (!is_callable($this->_observers[$event][$i])) {
-               call_user_func($this->_observers[$event][$i], &$this, $params;
+               call_user_func($this->_observers[$event][$i], &$this, $params);
                 // no error push here, because it should be pushed by the handler
                 $success = false;
             }
