59565df646302d02fc7671f618ca4f705121df01
Lukas Smith <lsmith@php.net>
2004-12-12 21:25:56 +0000

    - added new default for config arrays inside static methods so we can fetch them from the admin classes)
    git-svn-id: http://svn.php.net/repository/pear/packages/LiveUser/trunk@174548 c90b9560-bf6c-de11-be94-00142212c4b1

--- Perm/Storage/SQL.php
+++ Perm/Storage/SQL.php
@@ -92,12 +92,26 @@ class LiveUser_Perm_Storage_SQL extends LiveUser_Perm_Storage
             'group_define_name' => array('type' => 'text', 'name' => 'group_define_name')
         ),
         'optional' => array(
-            'group_type'    => array('type' => 'integer', 'name' => 'group_type'),
-            'is_active'    => array('type' => 'boolean', 'name' => 'is_active'),
+             'group_type'    => array('type' => 'integer', 'name' => 'group_type'),
+             'is_active'    => array('type' => 'boolean', 'name' => 'is_active'),
             'owner_user_id'  => array('type' => 'integer', 'name' => 'owner_user_id'),
             'owner_group_id' => array('type' => 'integer', 'name' => 'owner_group_id')
         )
     );
+
+    /**
+     *
+     * @access public
+     * @var    array
+     */
+    var $tables = array();
+
+    /**
+     *
+     * @access public
+     * @var    array
+     */
+    var $fields = array();
 
     /**
      * Constructor
@@ -109,6 +123,196 @@ class LiveUser_Perm_Storage_SQL extends LiveUser_Perm_Storage
     function LiveUser_Perm_Storage_MDB2(&$confArray, &$storageConf)
     {
         $this->LiveUser_Perm_Storage($confArray, $storageConf);
+
+        $this->tables = LiveUser::arrayMergeClobber(LiveUser_Perm_Simple::getTableDefaults(), $this->tables);
+        $this->fields = LiveUser::arrayMergeClobber(LiveUser_Perm_Simple::getFieldDefaults(), $this->fields);
+    }
+
+    function getTableDefaults()
+    {
+        return array(
+            'perm_users' => array(
+                'fields' => array(
+                    'perm_user_id' => 'seq',
+                    'auth_user_id' => true,
+                    'auth_container_name' => true,
+                    'perm_type' => false,
+                 ),
+                'joins' => array(
+                    'userrights' => 'perm_user_id',
+                    'groupusers' => 'perm_user_id',
+                ),
+                'id' => 'perm_user_id',
+            ),
+            'userrights' => array(
+                'fields' => array(
+                    'perm_user_id' => true,
+                    'right_id' => true,
+                    'right_level' => false,
+                ),
+                'joins' => array(
+                    'perm_users' => 'perm_user_id',
+                    'rights' => 'right_id',
+                ),
+            ),
+            'rights' => array(
+                'fields' => array(
+                    'right_id' => 'seq',
+                    'area_id' => false,
+                    'right_define_name' => false,
+                ),
+                'joins' => array(
+                    'areas' => 'area_id',
+                    'userrights' => 'right_id',
+                    'grouprights' => 'right_id',
+                    'rights_implied' => array(
+                        'right_id' => 'right_id',
+                        'right_id' => 'implied_right_id',
+                    ),
+                    'translations' => array(
+                        'right_id' => 'section_id',
+                        LIVEUSER_SECTION_RIGHT => 'section_type',
+                    ),
+                ),
+                'id' => 'right_id',
+            ),
+            'rights_implied' => array(
+                'fields' => array(
+                    'right_id' => true,
+                    'implied_right_id' => true,
+                ),
+                'joins' => array(
+                    'rights' => array(
+                        'right_id' => 'right_id',
+                        'implied_right_id' => 'right_id',
+                    ),
+                ),
+            ),
+            'translations' => array(
+                'fields' => array(
+                    'section_id' => true,
+                    'section_type' => true,
+                    'name' => false,
+                    'description' => false,
+                ),
+                'joins' => array(
+                    'rights' => array(
+                        'section_id' => 'right_id',
+                        'section_type' => LIVEUSER_SECTION_RIGHT,
+                    ),
+                    'areas' => array(
+                        'section_id' => 'area_id',
+                        'section_type' => LIVEUSER_SECTION_AREA,
+                    ),
+                    'applications' => array(
+                         'section_id' => 'application_id',
+                         'section_type' => LIVEUSER_SECTION_APPLICATION,
+                    ),
+                    'groups' => array(
+                        'section_id' => 'group_id',
+                        'section_type' => LIVEUSER_SECTION_GROUP,
+                    ),
+                ),
+            ),
+            'areas' => array(
+                'fields' => array(
+                    'area_id' => 'seq',
+                    'application_id' => false,
+                    'area_define_name' => false,
+                ),
+                'joins' => array(
+                    'rights' => 'area_id',
+                    'applications' => 'application_id',
+                    'translations' => array(
+                        'area_id' => 'section_id',
+                        LIVEUSER_SECTION_AREA => 'section_type',
+                    ),
+                ),
+                'id' => 'area_id',
+            ),
+            'applications' => array(
+                'fields' => array(
+                    'application_id' => 'seq',
+                    'application_define_name' => false,
+                ),
+                'joins' => array(
+                    'areas' => 'application_id',
+                    'translations' => array(
+                        'application_id' => 'section_id',
+                        LIVEUSER_SECTION_APPLICATION => 'section_type',
+                    ),
+                ),
+                'id' => 'application_id',
+            ),
+            'groups' => array(
+                'fields' => array(
+                    'group_id' => 'seq',
+                    'group_type' => false,
+                    'group_define_name' => false,
+                    'is_active' => false,
+                    'owner_user_id' => false,
+                    'owner_group_id' => false,
+                ),
+                'joins' => array(
+                    'groupusers' => 'group_id',
+                    'grouprights' => 'group_id',
+                    'translations' => array(
+                        'group_id' => 'section_id',
+                        LIVEUSER_SECTION_GROUP => 'section_type',
+                    ),
+                ),
+                'id' => 'group_id',
+            ),
+            'groupusers' => array(
+                'fields' => array(
+                    'perm_user_id' => true,
+                    'group_id' => true,
+                ),
+                'joins' => array(
+                    'groups' => 'group_id',
+                    'perm_users' => 'perm_user_id',
+                ),
+            ),
+            'grouprights' => array(
+                'fields' => array(
+                    'group_id' => true,
+                    'right_id' => true,
+                    'right_level' => false,
+                ),
+                'joins' => array(
+                    'rights' => 'right_id',
+                    'groups' => 'group_id',
+                ),
+            ),
+        );
+    }
+
+    function getFieldDefaults()
+    {
+        return array(
+            'perm_user_id' => 'integer',
+            'auth_user_id' => 'integer',
+            'auth_container_name' => 'text',
+            'perm_type' => 'integer',
+            'right_id' => 'integer',
+            'right_level' => 'integer',
+            'area_id' => 'integer',
+            'application_id' => 'integer',
+            'right_define_name' => 'text',
+            'area_define_name' => 'text',
+            'application_define_name' => 'text',
+            'section_id' => 'integer',
+            'section_type' => 'integer',
+            'name' => 'text',
+            'description' => 'text',
+            'group_id' => 'integer',
+            'group_type' => 'integer',
+            'group_define_name' => 'text',
+            'is_active' => 'boolean',
+            'owner_user_id' => 'integer',
+            'owner_group_id' => 'integer',
+            'implied_right_id' => 'integer',
+        );
     }
 
     /**
