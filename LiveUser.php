6ad719d9510800c3e19a8c896953d50769bbf1f5
Lukas Smith <lsmith@php.net>
2005-02-07 16:59:53 +0000

    - typo fix (upgrade before testing fool)
    git-svn-id: http://svn.php.net/repository/pear/packages/LiveUser/trunk@179113 c90b9560-bf6c-de11-be94-00142212c4b1

--- LiveUser.php
+++ LiveUser.php
@@ -896,7 +896,7 @@ class LiveUser
 
         if ($this->isLoggedIn()) {
             // user wants to logout or authenticate with new credentials
-            if ($logout || $handle)) {
+            if ($logout || $handle) {
                 $this->logout($logout);
             // Check if user authenticated with new credentials
             } elseif ($handle && $this->_auth->handle != $handle) {
