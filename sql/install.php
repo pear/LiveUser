a043f4b01ef748748278f54b1ed4403cedcb6cb6
Lukas Smith <lsmith@php.net>
2005-04-14 11:08:49 +0000

    - improved handling of defaults - improved DB seq support - options can now be passed to the installer
    git-svn-id: http://svn.php.net/repository/pear/packages/LiveUser/trunk@184329 c90b9560-bf6c-de11-be94-00142212c4b1

--- sql/install.php
+++ sql/install.php
@@ -42,13 +42,12 @@
 require_once 'LiveUser.php';
 require_once 'MDB2/Schema.php';
 
-/*
 $dsn = 'mysql://root:@localhost/liveuser_test_installer';
 
 $conf = array(
     'authContainers' => array(
         array(
-            'type'          => 'DB',
+            'type'          => 'MDB2',
             'dsn'           => $dsn,
             'authTable'     => 'liveuser_users',
             'authTableCols' => array(
@@ -58,102 +57,105 @@ $conf = array(
                     'passwd'       => array('name' => 'passwd',     'type' => 'text'),
                 ),
                 'optional' => array(
-                    'lastlogin'    => array('name' => 'lastLogin',  'type' => 'timestamp'),
-                    'is_active'    => array('name' => 'isActive',   'type' => 'boolean')
+                    'owner_user_id'  => array('name' => 'owner_user_id',    'type' => 'integer'),
+                    'owner_group_id' => array('name' => 'owner_group_id',   'type' => 'integer'),
+                    'lastlogin'      => array('name' => 'lastLogin',        'type' => 'timestamp'),
+                    'is_active'      => array('name' => 'isActive',         'type' => 'boolean')
                 ),
             ),
         ),
     ),
     'permContainer'  => array(
         'type'  => 'Complex',
-        'storage' => array('DB' => array('dsn' => $dsn, 'prefix'     => 'liveuser_')),
+        'storage' => array('MDB2' => array('dsn' => $dsn, 'prefix' => 'liveuser_')),
     )
 );
 
 $installer =& new LiveUser_Misc_Schema_Install();
-$result = $installer->installAuthSchema($conf['authContainers'][0], 'auth_mdb_schema.xml', true);
+
+$options = array(
+    'debug' => true,
+    'log_line_break' => '<br>',
+    'portability' => (MDB2_PORTABILITY_ALL ^ MDB2_PORTABILITY_EMPTY_TO_NULL),
+);
+
+$result = $installer->installAuthSchema($conf['authContainers'][0], 'auth_mdb_schema.xml', true, $options);
 var_dump($result);
-$result = $installer->installPermSchema($conf['permContainer']['storage']['DB'], 'perm_mdb_schema.xml', false);
+$result = $installer->installPermSchema($conf['permContainer']['storage'], 'perm_mdb_schema.xml', false, $options);
 var_dump($result);
-*/
 
 class LiveUser_Misc_Schema_Install
 {
-    function installAuthSchema($config, $file, $create = true)
+    function installAuthSchema($config, $file, $create = true, $options = array())
     {
-        if (isset($config['connection'])) {
-            $dsn = $config['connection']->dsn;
-            if (!isset($dsn['database'])) {
-                $dsn['database'] = $config['connection']->database_name;
-            }
+        $auth =& LiveUser::authFactory($config, 'foo');
+        if (!$auth) {
+            return false;
+        }
+
+        $dsn = $auth->dbc->dsn;
+        if (is_a($auth->dbc, 'DB_Common')) {
+            $options['sequence_col_name'] = 'id';
         } else {
-            $dsn = MDB2::parseDSN($config['dsn']);
+            $dsn['database'] = $auth->dbc->database_name;
         }
 
         $variables = array('database' => $dsn['database']);
 
-        $variables['user_table_name'] = isset($config['authTable'])
-            ? $config['authTable'] : 'liveuser_users';
-        $variables['auth_user_id_name'] = isset($config['authTableCols']['required']['auth_user_id']['name'])
-            ? $config['authTableCols']['required']['auth_user_id']['name'] : 'auth_user_id';
-        $variables['handle_name'] = isset($config['authTableCols']['required']['handle']['name'])
-            ? $config['authTableCols']['required']['handle']['name'] : 'handle';
-        $variables['passwd_name'] = isset($config['authTableCols']['required']['passwd']['name'])
-            ? $config['authTableCols']['required']['passwd']['name'] : 'passwd';
-        $variables['lastlogin_name'] = isset($config['authTableCols']['optional']['lastlogin']['name'])
-            ? $config['authTableCols']['optional']['lastlogin']['name'] : 'lastlogin';
-        $variables['is_active_name'] = isset($config['authTableCols']['optional']['is_active']['name'])
-            ? $config['authTableCols']['optional']['is_active']['name'] : 'is_active';
-        $variables['owner_user_id_name'] = isset($config['authTableCols']['custom']['owner_user_id']['name'])
-            ? $config['authTableCols']['custom']['owner_user_id']['name'] : 'owner_user_id';
-        $variables['owner_group_id_name'] = isset($config['authTableCols']['custom']['owner_group_id']['name'])
-            ? $config['authTableCols']['custom']['owner_group_id']['name'] : 'owner_group_id';
+        $variables['user_table_name'] = $auth->authTable;
+        $variables['auth_user_id_name'] = $auth->authTableCols['required']['auth_user_id']['name'];
+        $variables['handle_name'] = $auth->authTableCols['required']['handle']['name'];
+        $variables['passwd_name'] = $auth->authTableCols['required']['passwd']['name'];
+        $variables['lastlogin_name'] = $auth->authTableCols['optional']['lastlogin']['name'];
+        $variables['is_active_name'] = $auth->authTableCols['optional']['is_active']['name'];
+        $variables['owner_user_id_name'] = $auth->authTableCols['optional']['owner_user_id']['name'];
+        $variables['owner_group_id_name'] = $auth->authTableCols['optional']['owner_group_id']['name'];
 
-        return $this->installSchema($dsn, $file, $variables, $create);
+        return $this->installSchema($dsn, $file, $variables, $create, $options);
     }
 
-    function installPermSchema($config, $file, $create = true)
+    function installPermSchema($config, $file, $create = true, $options = array())
     {
-        if (isset($config['connection'])) {
-            $dsn = $config['connection']->dsn;
-            if (!isset($dsn['database'])) {
-                $dsn['database'] = $config['connection']->database_name;
-            }
+        $perm =& LiveUser::storageFactory($config);
+        if (!$perm) {
+            return false;
+        }
+
+        $dsn = $perm->dbc->dsn;
+        if (is_a($perm->dbc, 'DB_Common')) {
+            $options['sequence_col_name'] = 'id';
         } else {
-            $dsn = MDB2::parseDSN($config['dsn']);
+            $dsn['database'] = $perm->dbc->database_name;
         }
 
         $variables = array(
             'database' => $dsn['database'],
-            'table_prefix' => $config['prefix'],
+            'table_prefix' => $perm->prefix,
             'right_max_level' => LIVEUSER_MAX_LEVEL,
         );
 
-        return $this->installSchema($dsn, $file, $variables, $create);
+        return $this->installSchema($dsn, $file, $variables, $create, $options);
     }
 
-    function installSchema($dsn, $file, $variables, $create = true)
+    function installSchema($dsn, $file, $variables, $create = true, $options = array())
     {
         $manager =& new MDB2_Schema;
 
         unset($dsn['database']);
 
-        $options = array(
-            'debug' => true,
-            'log_line_break' => '<br>',
-            'portability' => (MDB2_PORTABILITY_ALL ^ MDB2_PORTABILITY_EMPTY_TO_NULL),
-        );
-
         $err = $manager->connect($dsn, $options);
         if (MDB2::isError($err)) {
-            return $err->getMessage().' - '.$err->getUserinfo();
+            return $err;
         }
 
         $variables['create'] = (int)$create;
-        $result = $manager->updateDatabase($file, 'old_'.$file, $variables);
-        echo('Debug messages<br>');
-        echo($manager->db->debugOutput().'<br>');
+        $result = $manager->updateDatabase($file, $file.'.old', $variables);
 
+        $debug = $manager->getOption('debug');
+        if ($debug && !PEAR::isError($debug)) {
+            echo('Debug messages<br>');
+            echo($manager->db->debugOutput().'<br>');
+        }
         $manager->disconnect();
         return $result;
     }
