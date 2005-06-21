d816c4dd02b0248d6dabfa659113637b391e7a18
Lukas Smith <lsmith@php.net>
2005-06-21 09:32:35 +0000

    - updated example install guidelines
    git-svn-id: http://svn.php.net/repository/pear/packages/LiveUser/trunk@188924 c90b9560-bf6c-de11-be94-00142212c4b1

--- docs/examples/example5/index.php
+++ docs/examples/example5/index.php
@@ -7,30 +7,21 @@
 </head>
 
 <body>
-
-    <div class="content">
-        <h1>LiveUser Example 5</h1>
-        <p>Please edit conf.php according to your needs and run the sql dump
-        contained in <br />
-        <code>
-            PEAR_DATA_DIRECTORY/liveuser/misc/schema/Auth_DB.sql &amp;
-            PEAR_DATA_DIRECTORY/liveuser/misc/schema/perm_db_(simple|medium).sql &amp;
-            PEAR_DATA_DIRECTORY/liveuser/docs/examples/examples5/create_db.sql
-        </code>
-        
-        <br /> on a database you created beforehand.</p>
-        <p>You can use the following commands :</p>
-        <p><code>#mysql -uUSER -p<br />
-        <span style="font-style: italic">enter password here</span><br />
-        mysql> create database DATABASENAME;<br />
-        mysql>exit;<br />
-        #mysql -uUSER -p DATABASENAME < PEAR_DATA_DIRECTORY/liveuser/misc/schema/create_db.sql
-    #mysql -uUSER -p DATABASENAME < PEAR_DATA_DIRECTORY/liveuser/docs/examples/examples5/create_db.sql   
-    </code>
-        <br />
-        <span style="font-style: italic">enter your password here. Writing it on the same line means anybody with access
-        to the server can see the password (by running #w for example)</span></p>
-    </div>
+    <h1>Test for the LiveUser class</h1>
+    <p>This example is intended to be used with the Auth/MDB2 and the Perm/MDB2_Complex driver.</p>
+    <p>To set this up follow these steps:</p>
+    <ol>
+        <li>Copy the files in this directory into your web root.</li>
+        <li>Configure your DSN and the PEAR path in the conf.php.</li>
+        <li>Make sure you read README in the examples root folder.</li>
+        <li>Importing demodata.xml to your test database using the demodata.php script</li>
+    </ol>
+    <p>If you want to test this example in your local path with out installing LiveUser in PEAR on
+    your server then uncomment these two lines in <b>main.inc.php</b> and point $path_to_liveuser_dir to live user dir :)<br />
+    <br />
+    $path_to_liveuser_dir = './pear/'.PATH_SEPARATOR;<br />
+    ini_set('include_path', $path_to_liveuser_dir.ini_get('include_path'));<br />
+    </p>
 
     <div class="content">
         <h2><a href="home.php">Proceed to example 5</a></h2>
