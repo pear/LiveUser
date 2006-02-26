78180212e6c5ff35323c706b266498f7ec0f4023
Lukas Smith <lsmith@php.net>
2006-02-26 11:32:06 +0000

    - updated for release
    git-svn-id: http://svn.php.net/repository/pear/packages/LiveUser/trunk@208008 c90b9560-bf6c-de11-be94-00142212c4b1

--- package.php
+++ package.php
@@ -9,88 +9,16 @@
 require_once 'PEAR/PackageFileManager.php';
 require_once 'Console/Getopt.php';
 
-$version = '0.16.9';
+$version = '0.16.10';
 
 $notes = <<<EOT
-This releases fixes a minor security issue that is limited to the optional
-remember me feature. This issue was report to us by GulfTech Security Research.
-
-The issue would allow an attacker to determine the existance of files inside the
-file system, as well as being able to delete files:
-- if the relativ path is shorter than 32 characters (including a null
-byte)
-- if null bytes are handled inside the "_COOKIE" superglobal, for example
-through usage of magic_quotes_gpc, the issue becomes essentially limited to
-files ending with ".lu".
-
-All installations using the remember me feature are strongly urged to update.
-This release also changes some other aspects including a BC break so developers
-can optionally patch their current installations from the changes in the
-following commit:
-http://cvs.php.net/viewcvs.cgi/pear/LiveUser/LiveUser.php?r1=1.148&r2=1.149&diff_format=u
-
-- fixed major bug in PEARAuth container: auth_user_id is not an optional property
-- added passwordEncryptionMode and secret to phpdoc comment
-- made cryptRC4() method public to match usage in auth common in the client and admin api
-- fixed handling of the secret user defineable property (bug #6551)
-- added support for user_group_ids (bug #6517)
-- allow grouprights and groupusers table to join eachother
-- updateProperty doesn't update the session (bug #6612)
-- renamed "connection" config option to "dbc" *BC BREAK*
-- cleaned up and unified init() in the storage classes
-- added example for dumping SQL to a file to installer
-- add support for force_seq to installer
-- removed allowDuplicateHandles and allowEmptyPasswords options, they are now
-  handled through the table definition in the given Globals.php (overwriteable
-  via the config array) *BC BREAK*
-- initial untested support for PDO in the installer
-- added examples for setting length and defaults to installer
-- use overwrite when unlink is enabled in the installer
-- reworked handling of merging user with group rights *BC BREAK*
-When using the Medium or Complex container a user may gain rights through direct
-assignment or through membership in a group that has rights assigned. The user
-and group rights are merged with the following logic:
-* if the right is only assigned to a member group but not the user the right is
-  available to the user at the level at which the group has the right
-* if the right is only assigned to the user at a level greater than zero but not
-  to a member group the right is available to the user at the level at which
-  user has the right
-* if the right is only assigned to the user at a level equal to zero but not
-  to a member group the right is available to the user at the level at which
-  user has the right
-* if the right is only assigned to the user at a level lower than zero but not
-  to a member group then the right is unavailable to the user
-* if the is assigned to a member group and the user and the level at which the
-  user has the right is greater than zero, then the right is available to the
-  user at higher level of the two
-* if the is assigned to a member group and the user and the level at which the
-  user has the right is equal to zero, then the right is unavailable to the user
-* if the is assigned to a member group and the user and the level at which the
-  user has the right is lower than zero, then the right is available to the
-  user at the minimum of the group assigned level and the addition of the
-  negativ user level and the maximum level
-Example:
-The user as the following right_id => level pairs
-array
-  1 => 3
-  2 => -2
-  3 => 0
-  5 => -1
-
-The groups he is a member of have the following right_id => level pairs
-array
-  1 => 1
-  2 => 3
-  3 => 3
-  4 => 2
-
-The final right_id => level pairs are as follows
-array
-  1 => 3 // user has a higher level (3) than the group level (1)
-  2 => 1 // 3 - 2 means a maximum possible level of 1
-  4 => 2 // only group has the right at level 2
-  5 => 2 // only user has the right at level 3 - 1 = 2
-
+- Do not include Cache.php since its only a concept and not implemented yet
+- fixed seriously issue with right reading when a user only has group and no
+  direct rights assigned
+- right_level may not be null (use default if not explicitly set)
+- phpdoc improvements
+- bumped dependency for MDB2 to first stable release
+- added optional dependency on mcrypt
 EOT;
 
 $description = <<<EOT
@@ -133,7 +61,7 @@ $result = $package->setOptions(array(
     'state'             => 'beta',
     'license'           => 'LGPL',
     'filelistgenerator' => 'cvs',
-    'ignore'            => array('package.php', 'package.xml', 'TODO', 'DefineGenerator'),
+    'ignore'            => array('package.php', 'package.xml', 'TODO', 'DefineGenerator', 'Cache.php'),
     'notes'             => $notes,
     'changelogoldtonew' => false,
     'simpleoutput'      => true,
@@ -167,16 +95,17 @@ $package->addMaintainer('lsmith',  'lead',      'Lukas Kahwe Smith', 'smith@poot
 $package->addMaintainer('krausbn', 'developer', 'Bjoern Kraus',      'krausbn@php.net');
 $package->addMaintainer('dufuz',   'lead',      'Helgi Þormar',     'dufuz@php.net');
 
-$package->addDependency('php',              '4.2.0',    'ge',  'php', false);
-$package->addDependency('PEAR',             '1.3.3',    'ge',  'pkg', false);
-$package->addDependency('Event_Dispatcher', false,      'has', 'pkg', false);
-$package->addDependency('Log',              '1.7.0',    'ge',  'pkg', true);
-$package->addDependency('DB',               '1.6.0',    'ge',  'pkg', true);
-$package->addDependency('MDB',              '1.1.4',    'ge',  'pkg', true);
-$package->addDependency('MDB2',             '2.0.0RC1', 'ge',  'pkg', true);
-$package->addDependency('MDB2_Schema',      false,        'has', 'pkg', true);
-$package->addDependency('XML_Tree',         false,        'has', 'pkg', true);
-$package->addDependency('Crypt_RC4',        false,        'has', 'pkg', true);
+$package->addDependency('php',              '4.2.0', 'ge',  'php', false);
+$package->addDependency('PEAR',             '1.3.3', 'ge',  'pkg', false);
+$package->addDependency('Event_Dispatcher', false,   'has', 'pkg', false);
+$package->addDependency('Log',              '1.7.0', 'ge',  'pkg', true);
+$package->addDependency('DB',               '1.6.0', 'ge',  'pkg', true);
+$package->addDependency('MDB',              '1.1.4', 'ge',  'pkg', true);
+$package->addDependency('MDB2',             '2.0.0', 'ge',  'pkg', true);
+$package->addDependency('MDB2_Schema',      false,   'has', 'pkg', true);
+$package->addDependency('XML_Tree',         false,   'has', 'pkg', true);
+$package->addDependency('Crypt_RC4',        false,   'has', 'pkg', true);
+$package->addDependency('mcrypt',           false,   'has', 'ext', true);
 
 if (array_key_exists('make', $_GET) || (isset($_SERVER['argv'][1]) && $_SERVER['argv'][1] == 'make')) {
     $result = $package->writePackageFile();
