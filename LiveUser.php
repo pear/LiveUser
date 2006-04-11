9012d954503d50d62e7e0a01cfb8ff8cdc47b036
Lukas Smith <lsmith@php.net>
2006-04-11 13:34:29 +0000

    - fixed parse error
    git-svn-id: http://svn.php.net/repository/pear/packages/LiveUser/trunk@211199 c90b9560-bf6c-de11-be94-00142212c4b1

--- LiveUser.php
+++ LiveUser.php
@@ -704,7 +704,7 @@ class LiveUser
      */
     function classExists($classname)
     {
-        if (version_compare(phpversion(), "5.0", ">=") {
+        if (version_compare(phpversion(), "5.0", ">=")) {
             return class_exists($classname, false);
         }
         return class_exists($classname);
